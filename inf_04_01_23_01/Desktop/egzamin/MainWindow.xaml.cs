dd21d7e492ef12a5d0b3e9abfae399e39fc976ef
Patryk Przybył <144985546+przybylpatryk@users.noreply.github.com>
2026-09-06 18:54:45 +0200

    dodaj pracownika

---
+++ inf_04_01_23_01/Desktop/egzamin/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace egzamin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string finalPassword = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            finalPassword = "";
+            string lowerCase = "abcdefghijklmnopqrstuvwxyz";
+            string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string numbers = "0123456789";
+            string specialChars = "!@#$%^&*()_+-=";
+
+            int passwordLength = int.Parse(txtLength.Text);
+
+            Random random = new Random();
+
+            if (cbUpperLower.IsChecked == true)
+            {
+                finalPassword += upperCase[random.Next(upperCase.Length)];
+            }
+
+            if (cbNums.IsChecked == true)
+            {
+                finalPassword += numbers[random.Next(numbers.Length)];
+            }
+
+            if (cbChars.IsChecked == true)
+            {
+                finalPassword += specialChars[random.Next(specialChars.Length)];
+            }
+
+            while (finalPassword.Length < passwordLength)
+            {
+
+                finalPassword += lowerCase[random.Next(lowerCase.Length)];
+            }
+
+            MessageBox.Show(finalPassword);
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            string surname = txtSurname.Text;
+            ComboBoxItem cbItem = (ComboBoxItem)cbBox.SelectedItem;
+            string position = cbItem.Content.ToString();
+
+            MessageBox.Show($"Dane pracownika: {name} {surname} {position} Hasło: {finalPassword}");
+        }
+    }
+}
---
+++ inf_04_01_23_01/Konsola/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Konsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint a, b;
+
+            Console.WriteLine("Podaj a:");
+            a = uint.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj b:");
+            b = uint.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nNWD:");
+            Console.WriteLine(Euclides(a, b));
+        }
+
+        public static uint Euclides(uint a, uint b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+                }
+            }
+            return a;
+        }
+    }
+}
