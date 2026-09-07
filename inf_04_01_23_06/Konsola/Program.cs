aef0ecb8ac93c9a276c5151e641c235391a4686f
Patryk Przybył <144985546+przybylpatryk@users.noreply.github.com>
2026-09-07 21:01:04 +0200

    sito

---
+++ inf_04_01_23_06/Konsola/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Konsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = new int[101];
+            fillNumArray(numArray);
+
+            bool[] A = new bool[numArray.Length];
+            fillBoolA(A);
+
+            for (int i = 2; i <= Math.Sqrt(numArray.Length); i++)
+            {
+                if (A[i] == true)
+                {
+                    for (int j = i + i; j < numArray.Length; j += i)
+                    {
+                        A[j] = false;
+                    }
+                }
+            }
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == true)
+                {
+                    Console.WriteLine("Kolejna liczba pierwsza: " + i);
+                }
+            }
+        }
+
+        static void fillNumArray(int[] array)
+        {
+            for (int i = 2; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+        }
+
+        static void fillBoolA(bool[] A)
+        {
+            for (int i = 2; i < A.Length; i++)
+            {
+                A[i] = true;
+            }
+        }
+    }
+}
